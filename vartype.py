2c32f53a39f1b9ad5c99843098baa3b3aee53828
koptelova  margarita <koptelovam2008@gmail.com>
2025-06-06 21:41:56 +0300

    vartype

---
+++ vartype.py
@@ -0,0 +1,8 @@
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+print("Сумма:", num1 + num2)
+user_input = input("Введите целое число: ")
+converted_num = int(user_input)
+print("Тип данных после преобразования:", type(converted_num))
+age = input("Сколько вам лет? ")
+print(f"Вам {age} лет.")
