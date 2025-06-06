acbb14f0b8e3e0f5ffc888918cb6e9acc243ef87
koptelova  margarita <koptelovam2008@gmail.com>
2025-06-07 00:06:31 +0300

    mylibs

---
+++ mylibs.py
@@ -0,0 +1,13 @@
+#27
+import random
+random_number = random.randint(1, 100)
+print(f"Случайное число: {random_number}")
+#28
+from datetime import datetime
+current_time = datetime.now()
+print("Текущая дата и время:", current_time)
+#29
+import math
+number = float(input("Введите число: "))
+sqrt_value = math.sqrt(number)
+print(f"Квадратный корень (с math): {sqrt_value:.4f}")
